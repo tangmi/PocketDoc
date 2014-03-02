0ab2d6b8b399b36c225aec00c5bdaa4fc07e46e9
Michael Tang <tangmi@uw.edu>
2014-03-01 21:42:48 -0800

    shit

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -35,6 +37,8 @@ app.get('/stylesheets/style.css', function(req, res) {
 		}
 	});
 });
+
+
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
