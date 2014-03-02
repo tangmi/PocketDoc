2108e81e302c13cea104026ea48b4aa63fb02e4e
Michael Tang <tangmi@uw.edu>
2014-03-01 21:37:31 -0800

    pbbbbbbt

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -12,8 +10,6 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -39,9 +35,6 @@ app.get('/stylesheets/style.css', function(req, res) {
 		}
 	});
 });
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
